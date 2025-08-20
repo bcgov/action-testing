abda41f594cc15d7c7723a7ac1652625631880a0
Isaac C <isaac1345@gmail.com>
2025-08-20 13:42:50 -0700

    Added ago login

--- arc/arc_test.py
+++ arc/arc_test.py
@@ -1,4 +1,17 @@
+
+import os
+
 import arcgis
+from arcgis.gis import GIS
 import geopandas as gpd
 
+
+username = os.getenv('AGO_USERNAME')
+password = os.getenv('AGO_PASSWORD')
+portal = os.getenv('AGO_PORTAL')
+
+gis = GIS(url=portal, username=username, password=password)
+
+print(username)
+print(f"logged in as {gis.properties.user.username} to {gis.properties.name} at {gis.properties.url}")
 print("Successfully imported arcgis and geopandas")
